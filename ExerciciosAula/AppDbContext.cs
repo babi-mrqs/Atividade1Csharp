976e2a7e9716b9cb7379a453b8d945b837ebe78d
Babi <146398580+babi-mrqs@users.noreply.github.com>
2024-06-26 21:17:40 -0300

    Arquivos via upload
    Criação do projeto e winFormsApp
    Criação de botões para abrir novas janelas e botões para fechar janelas

---
+++ ExerciciosAula/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosAula
+{
+    public class AppDbContext : DbContext
+    {
+            public DbSet<Usuario> Usuarios { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder
+    optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=exercicios.db");
+            }
+
+    }
+    
+}
---
+++ ExerciciosAula/FrmUsuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciciosAula
+{
+    public partial class FrmUsuario : Form
+    {
+        public FrmUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoltarUsuario_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLoginUsuario_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
